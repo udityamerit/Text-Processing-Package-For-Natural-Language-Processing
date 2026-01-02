106525b16382bd3e13eb4adbf575fb3863bb79da
Uditya Narayan Tiwari <uditmerit@gmail.com>
2026-01-03 00:10:47 +0530

    update

--- setup.py
+++ setup.py
@@ -3,14 +3,13 @@ import setuptools
 with open('Readme.md') as fp:
     long_description = fp.read()
 
-# Use splitlines() to create a list of requirements
 with open('requirements.txt') as fp:
     requirements = fp.read().splitlines()
 
 setuptools.setup(
     name='nlp_text_preprocessing',
     include_package_data=True,
-    version='0.0.3',
+    version='0.0.4', 
     author='Uditya Narayan Tiwari',
     author_email='tiwarimerit@gmail.com',
     description='This is a Text Processing Package For NLP',
@@ -22,6 +21,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent"
     ],
-    
-    python_requires='>=3.10',
+ 
+    install_requires=requirements, 
+    python_requires='>=3.9',
 )
